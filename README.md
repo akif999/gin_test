82ca9071a50d5a2e941bacab323415c043ecf14d
akif999 <aki01nf28@gmail.com>
2021-08-17 23:51:18 +0900

    クエリの値を利用するサンプルを追加

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ It is example making API with gin.
 This API server can be accessed with following command.
 
 ```
+# response = {"status": "ok"}
 $ curl 127.0.0.1:3000/v1/api1
+
+# response = {"status": "1234"}
+$ curl 127.0.0.1:3000/v1/api2?hoge=1234
 ```
--- main.go
+++ main.go
@@ -9,14 +9,25 @@ import (
 func main() {
 	engine := gin.Default()
 
+	// API の定義
 	v1 := engine.Group("/v1")
 	v1.GET("/api1", api1Handle)
+	v1.GET("/api2", api2Handle)
 
 	engine.Run(":3000")
 }
 
+// クエリの値は利用しない API
 func api1Handle(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"status": "ok",
+	})
+}
+
+// クエリの値を利用する API
+func api2Handle(c *gin.Context) {
+	hoge := c.Query("hoge")
+	c.JSON(http.StatusCreated, gin.H{
+		"status": hoge,
 	})
 }
